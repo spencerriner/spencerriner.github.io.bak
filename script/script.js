cc54a6a36c7ba9c43827c9fba6e4174b7cdbd277
Spencer Riner <spencerriner@gmail.com>
2015-06-04 00:06:40 -0500

    v6 - jq popups
    - added jquery popups to icons
    - cleaned up LESS
    - made new HTML skeleton

--- script/script.js
+++ script/script.js
@@ -5,6 +5,31 @@ $( document ).ready(function() {
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
-});
+	});
+
+	$('#prog-popup').popup({
+		transition: 'all 0.3s',
+  		scrolllock: true 
+	});
+
+	$('#dev-popup').popup({
+		transition: 'all 0.3s',
+  		scrolllock: true 
+	});
+
+	$('#mobile-popup').popup({
+		transition: 'all 0.3s',
+  		scrolllock: true 
+	});
+
+	$('#music-popup').popup({
+		transition: 'all 0.3s',
+  		scrolllock: true 
+	});
+
+	$('#sr-popup').popup({
+		transition: 'all 0.3s',
+  		scrolllock: true 
+	});
  
 });
